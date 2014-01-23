98708874dbeadb51cb37dc510c26c90d5e9c5730
Joseph Benden <joe@thrallingpenguin.com>
2014-01-23 17:10:45 -0500

    Fix configuration for production env.

--- unicorn.rb
+++ unicorn.rb
@@ -1,3 +1,5 @@
-preload_app true   # Avoid regeneration of the site for each fork.
-timeout 30         # Restarts workers that hang for 30 seconds.
-worker_processes 4 # Amount of unicorn workers to spin up.
+preload_app true             # Avoid regeneration of the site for each fork.
+timeout 30                   # Restarts workers that hang for 30 seconds.
+worker_processes 4           # Amount of unicorn workers to spin up.
+pid "/var/run/benden_us.pid" # PID file of Unicorn
+user "www-data"              # Owner of processes
